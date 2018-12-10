d0c6667cca7e0052d756a9171e8f3b2b2f902560
Kristijan Nedanoski <kristijannedanoski@gmail.com>
2018-12-10 13:50:38 +0100

    First Commit. Default project created

---
+++ CloudData/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CloudData.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CloudData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudData.Global" Language="C#" %>
