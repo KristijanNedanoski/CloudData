2d76bb1211a7ebaf5ce4f93422adf5b407c37b2e
Kristijan Nedanoski <kristijannedanoski@gmail.com>
2018-12-10 14:06:19 +0100

    Second commit. Added File.cs, Category.cs and FileContext.cs

---
+++ CloudData/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CloudData.Models
+{
+    public class Category
+    {
+        [ScaffoldColumn(false)]
+        public int CategoryID { get; set; }
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string CategoryName { get; set; }
+        [Display(Name = "File Description")]
+        public string Description { get; set; }
+        public virtual ICollection<File> Products { get; set; }
+    }
+}
---
+++ CloudData/Models/File.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudData.Models
+{
+    public class File
+    {
+        [ScaffoldColumn(false)]
+        public int FileID { get; set; }
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string FileName { get; set; }
+        [Required, StringLength(10000), Display(Name = "File Description"),
+        DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+        public int? CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
